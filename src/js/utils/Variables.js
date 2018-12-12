4b16d3ddce82014a3360c71e440daa0f8cc3138e
Alph4rd <15257735+Alph4rd@users.noreply.github.com>
2018-12-12 12:08:42 -0200

    update to latest client(unstable)

--- src/js/utils/Variables.js
+++ src/js/utils/Variables.js
@@ -1,100 +1,106 @@
 class Variables {
-	static get heroUpdateShd() {
-		return "_-L2n"; 
-	}
-	
-	static get heroPetId() {
-		return "_-Nm";
-	}
-
-	static get selectMaxShd() {
-		return "_-34J"; 
-	}
-
-	static get heroInitMaxShd() {
-		return "_-34J"; 
-	}
-
 	static get assetCreateX() {
-		return "_-k4P";
+		return "_-xD";
 	}
 	
 	static get assetCreateY() {
-		return "_-S3o";
+		return "_-s4w";f
 	}
 	
 	static get boxType() {
-		return "_-O25";
+		return "_-Jg";
 	}
 	
 	static get gateId() {
-		return "_-HD";
+		return "_-F2";
 	}
 	
 	static get gateType() {
-		return "_-c1h";
+		return "_-g2M";
 	}
 	
 	static get attackerId() {
-		return "_-71l";
+		return "_-i8";
 	}
 	
 	static get heroAttackedId() {
-		return "_-15k";
+		return "_-j7";
 	}
 	
 	static get attackHp() {
-		return "_-lo";
+		return "_-g3C";
 	}
 	
 	static get attackShd() {
-		return "_-c3Q";
+		return "_-da";
 	}
+	
+	static get heroInitMaxHp() {
+		return "_-T3w";
+	}
+	
+	static get heroInitMaxShd() {
+		return "_-q4Z";
+	}
+	
+	static get heroInitHp() {
+		return "_-53m";
+	}
+	
+	static get heroPetId() {
+		return "_-E28";
+	}
+	
 	static get hpUpdateMaxHp() {
-		return "_-t6";
+		return "_-X1H";
 	}
 	
 	static get hpUpdateHp() {
-		return "_-32V";
+		return "_-24e";
 	}
 	
 	static get resource() {
-		return "_-5U";
+		return "_-Oi";
 	}
 	
 	static get attackerId() {
-		return "_-71l";
+		return "_-i8";
 	}
 	
 	static get attackedId() {
-		return "_-U1D";
+		return "_-G3Y";
 	}
 	
 	static get clanDiplomacy() {
-		return "_-V4D";
+		return "_-fA";
 	}
 	
 	static get shipDestroyedId() {
-		return "_-C1A";
+		return "_-l15";
 	}
 	
 	static get moveDuration() {
-		return "_-x1";
+		return "_-4G";
 	}
 	
 	static get selectMaxHp() {
-		return "_-t6";
+		return "_-X1H";
+	}
+	
+	static get selectMaxShd() {
+		return "_-q4Z";
 	}
 	
 	static get selectHp() {
-		return "_-32V";
+		return "_-24e";
 	}
+		
 	
 	static get resourceType() {
-		return "_-tq";
+		return "_-w1f";
 	}
 
 	static get battlestationClanDiplomacy() {
-		return "_-x2L"; 
+		return "_-mA"; 
 	}
 }
